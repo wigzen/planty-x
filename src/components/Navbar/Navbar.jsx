b01c452aedf0a518307da6a9fc25b52e1aa9b45a
wigzen <wigzen18@gmail.com>
2023-05-18 16:47:25 +0530

    fix responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from '../../assets/nav-logo.svg'
 import { FiSearch, FiUser } from 'react-icons/fi'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
+import { AiOutlineAlignRight } from 'react-icons/ai'
 import './navbar.css'
 import useModal from '../../hooks/useModal'
 import Modal from '../Modal/Modal'
@@ -31,6 +32,9 @@ const Navbar = () => {
         </li>
         <li className="navbar__icon">
           <HiOutlineShoppingCart size={18} />
+        </li>
+        <li className="navbar__icon mobile">
+          <AiOutlineAlignRight size={18} />
         </li>
       </ul>
       {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
